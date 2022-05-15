cb2831fa9c3127d53701d6ab6db6b0d61ce03793
leonardo <ingeniero.leonardo.morales@gmail.com>
2022-05-15 13:28:03 -0500

    tamplate-dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { LateralMenuComponent } from './shared/lateral-menu/lateral-menu.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LateralMenuComponent,
+    FooterComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule
